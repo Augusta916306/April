70f8f2969c0f2143090f48ebcefbc9a01d352afb
Augusta916306 <Augusta.okeke@edu.sait.ca>
2023-10-20 09:31:56 -0600

    Step 2

--- temp.py
+++ temp.py
@@ -1,4 +1,12 @@
-"""Python Statement to convert 25 degree celsius to farenheit"""
+"""Python Statement to convert degree celsius to fahrenheit"""
+
+"""Function to convert temperature from celsius to fahrenheit"""
+
+def celsius_to_farenheint(celsius):
+    """ Funtion to convert value given to fahrenheit"""
+    fah = celsius * 9/5 + 32
+    return fah
+
 celsius = 25
 fah = celsius * 9/5 + 32
-print(f"{25} celsius degree is {fah}farenheit")
+print(f"{25} celsius degree is {fah} farenheit")
